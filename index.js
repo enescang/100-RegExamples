ec20301695d93088ce9f36e36e85b95bb3b0570e
kodcanlisi <developerindiee@gmail.com>
2020-06-05 19:13:35 +0300

    Add 5 new examples (Char Rules)

--- index.js
+++ index.js
@@ -6,26 +6,26 @@
  * (Each examples is seperated)
  */
 
-//1 Match String (case sensitive)
+console.log("1 Match String (case sensitive)")
 {
   const str = "Hello Regex Examples";
   const str2 = "Hello regex Examples";
   const regex = /Regex/;
-  //console.log(regex.test(str)); //true
-  //console.log(regex.test(str2)); //false
+  console.log(regex.test(str)); //true
+  console.log(regex.test(str2)); //false
 }
 
-//2 Match String (case insensitive)
+console.log("2 Match String (case insensitive)")
 // /str/i "i" is ignoring the wheter case is upper or lower
 {
   const str = "Hello Regex Examples";
   const str2 = "Hello regex Examples";
   const regex = /Regex/i;
-  // console.log(regex.test(str)); //true
-  // console.log(regex.test(str2)); //true
+  console.log(regex.test(str)); //true
+  console.log(regex.test(str2)); //true
 }
 
-//3 Char set (case insesitive)
+console.log("3 Char set (case insesitive)");
 {
   const str = "axe";
   //if there is any string between f-k =>true so it is false
@@ -34,4 +34,53 @@
   const regex2 = /[a-z]/i;
   console.log(regex.test(str)); //false
   console.log(regex2.test(str)); //true
+}
+
+console.log("4: Number set")
+{
+  const str  = "one day 1 pc";
+  const str2 = "one day one pc";
+  const regex = /[0-9]/;
+  console.log(regex.test(str));//true
+  console.log(regex.test(str2));//false
+}
+
+console.log("5: Char rules (num-str-num)");
+{
+  const str = "1b2";
+  const str2 = "acb";
+  const regex = /[0-9][a-z][0-9]/
+  console.log(regex.test(str));//true
+  console.log(regex.test(str2));//false
+}
+
+console.log("6: Char rules 2 (str-str-num)");
+{
+  const str = "1b2";
+  const str2 = "acb9";
+  const regex = /[a-z][a-z][0-9]/
+  console.log(regex.test(str));//false
+  console.log(regex.test(str2));//true
+}
+
+console.log("7: Char Rules 3 ([a-z] and exact string");
+{
+  const str = "set";
+  const str2 = "get";
+  const str3 = "tat";
+  const regex = /[a-z]et/
+  console.log(regex.test(str))//true
+  console.log(regex.test(str2))//true
+  console.log(regex.test(str3))//false
+}
+
+console.log("8: Use or (c or x allowed)");
+{
+  const str = "ace";
+  const str2 = "axe";
+  const str3 = "ate";
+  const regex = /a[c|x]e/
+  console.log(regex.test(str))//true
+  console.log(regex.test(str2))//true
+  console.log(regex.test(str3))//false
 }
